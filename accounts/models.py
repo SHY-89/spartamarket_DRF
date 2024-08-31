db5ef537543c8483e3da420d54cc91a76022342f
SHY-89 <tjduwkrn@gmail.com>
2024-08-31 21:40:04 +0900

    start spartamarket_drf initial settings

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class User(AbstractUser):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    name = models.CharField(max_length="10")
+    nickname = models.CharField(max_length="10")
+    email = models.EmailField(_("email address"), unique=True)
+    birth_date = models.DateField()
+    gender = models.models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    memo = models.TextField(blank=True)
