5fccc99bb86f81f39622340adf28651815b07c2d
SHY-89 <tjduwkrn@gmail.com>
2024-09-02 19:41:41 +0900

    feat accounts
    login 수정
    logout 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 from . import views
@@ -8,7 +7,8 @@ from . import views
 
 app_name="accounts"
 urlpatterns = [
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", views.LoginAPIView.as_view(), name="login"),
+    path("logout/", views.LogoutAPIView.as_view(), name="logout"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", views.signup, name="signup"),
     path("<str:username>/", views.profil, name="profil"),
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,14 @@
 from django.http import JsonResponse
 from django.core import serializers
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenBlacklistView
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from .models import User
 from .serializers import UserSerializer
 
@@ -26,3 +31,39 @@ def profil(request, username):
         serializer = UserSerializer(user_info)
         return Response(serializer.data, status=200)
     return Response({"error":"본인계정만 조회가 가능합니다."}, status=400)
+
+
+class LoginAPIView(TokenObtainPairView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        res = super().post(request, *args, **kwargs)
+        data = {
+            "detail": "login success",
+            "refresh": res.data.get('refresh', None),
+            "access": res.data.get('access', None)
+        }
+        response = Response(data, status= status.HTTP_200_OK)
+        response.set_cookie("refresh", res.data.get('refresh', None), httponly= True)
+        response.set_cookie("access", res.data.get('access', None), httponly= True)
+
+        return response
+    
+
+class LogoutAPIView(TokenBlacklistView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        refresh_token = request.COOKIES.get('refresh', 'Not Token')
+        data = {"refresh": str(refresh_token)}
+        serializer = self.get_serializer(data= data)
+
+        try:
+            serializer.is_valid(raise_exception= True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        response = Response({"detail": "token blacklisted"}, status= status.HTTP_200_OK)
+        response.delete_cookie("refresh")
+        response.delete_cookie("access")
+
+        return response
